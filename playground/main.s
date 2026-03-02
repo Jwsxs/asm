035ddf6f05557bd1a396cb3215ddf29674f5c9d3
Jwsxs <felipe.jwsus@gmail.com>
2026-03-02 03:30:59 +0000

    Messing with input and output

--- playground/main.s
+++ playground/main.s
@@ -1,56 +1,50 @@
-	.file	"main.c"
-	.text
-	.globl	set
-	.type	set, @function
-set:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movq	%rdi, -8(%rbp)
-	movl	%esi, -12(%rbp)
-	movq	-8(%rbp), %rax
-	movl	-12(%rbp), %edx
-	movl	%edx, (%rax)
-	nop
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	set, .-set
-	.globl	main
-	.type	main, @function
-main:
-.LFB1:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movq	%fs:40, %rax
-	movq	%rax, -8(%rbp)
-	xorl	%eax, %eax
-	leaq	-12(%rbp), %rax
-	movl	$5, %esi
-	movq	%rax, %rdi
-	call	set
-	movl	$0, %eax
-	movq	-8(%rbp), %rdx
-	subq	%fs:40, %rdx
-	je	.L4
-	call	__stack_chk_fail@PLT
-.L4:
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE1:
-	.size	main, .-main
-	.ident	"GCC: (GNU) 15.2.1 20260209"
-	.section	.note.GNU-stack,"",@progbits
+	.DATA
+MSG:	.ASCII "Hello, what's your name?\n"
+MSG_LEN = . - MSG # GET MSG LENGTH
+
+	.BSS
+.LCOMM BUFFER, 64
+	
+	.TEXT
+	.GLOBL _start
+	
+_start:
+	PUSH %RBP
+	MOVQ %RSP, %RBP
+
+	MOV $MSG, %RSI
+	MOV $MSG_LEN, %RDX
+	CALL PRINT_RSI
+
+	CALL GET_NAME
+
+	MOV $MSG, %RSI
+	MOV $7, %RDX
+	CALL PRINT_RSI
+	
+	MOV %R8, %RSI
+	MOV $64, %RDX
+	CALL PRINT_RSI
+	
+	MOV $60, %RAX
+	# MOV $0, %RDI
+	XOR %RDI, %RDI
+	SYSCALL
+
+PRINT_RSI:
+	MOV $1, %RAX
+	MOV $1, %RDI
+	SYSCALL
+	RET
+	
+GET_NAME:
+	MOV $0, %RAX
+	MOV $0, %RDI
+	MOV $BUFFER, %RSI
+	MOV $64, %RDX
+	SYSCALL
+
+	MOV %RSI, %R8
+	MOV $64, %R9
+
+	RET
