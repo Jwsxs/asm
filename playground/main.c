bf44cfabf71909e1277e3c41183ec8be92c84b22
Jwsxs <felipe.jwsus@gmail.com>
2026-03-12 14:46:45 +0000

    Fixed array_creation_test

--- playground/main.c
+++ playground/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
 int main() {
-	int x[132]; // separa o espaço
-
-	for (int i = 0; i < sizeof(x) / sizeof(x[0]);  i++) { // itera entre cada um
-		printf("Number %i\t--\t%d -> %d\n", i + 1, x[i], (x[i] % 2 == 0) ? 1 : 0); // vê se é par
-	}
-
+	float x = 2.3;
 	return 0;
 }
